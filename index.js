05f920b1945e04feba8bafd8c190a720613e0094
Sat <124409014+SatLess@users.noreply.github.com>
2025-12-19 11:45:43 -0300

    Função de salvar e scrape tabela

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const { tableParser } = require('puppeteer-table-parser')
+const file_saver = require('node:fs');
 puppeteer.use(StealthPlugin());
 
 const SELECTOR_GRADUACAO = "select[id='formTurma:inputNivel']";
@@ -7,17 +9,35 @@ const SELECTOR_UNIDADE = "select[id='formTurma:inputDepto']";
 const SELECTOR_PERIODO = "select[id='formTurma:inputPeriodo']";
 const SELECTOR_BUSCAR = "input[name='formTurma:j_id_jsp_1370969402_11']";
 const SELECTOR_TABELA = "table[class='listagem']"
-let lista_unidades = [];
+let qtd_unidades = 0
 
 async function scrape_disciplinas(unidade_idx, page) {
+    
     await page.waitForSelector(SELECTOR_BUSCAR)
     await page.waitForSelector(SELECTOR_UNIDADE)
-    await page.evaluate((selector_unidade, selector_buscar, idx) => {
-        document.querySelector(selector_unidade).selectedIndex = idx
+    const nome_unidade = await page.evaluate((selector_unidade, selector_buscar, idx) => {
+        let unidade = document.querySelector(selector_unidade)
+        unidade.selectedIndex = idx
+        let nome = unidade.options[idx].text
         document.querySelector(selector_buscar).click(); 
+        return nome
 }, SELECTOR_UNIDADE,SELECTOR_BUSCAR, unidade_idx)
     await page.waitForNavigation()
-    await tableToCSV(page)
+    const allowed = await page.evaluate((selector) => document.querySelector(selector) != null, SELECTOR_TABELA)
+    if (allowed === false) {return}
+    const data = await tableParser(page, {
+      selector: SELECTOR_TABELA,
+      allowedColNames: {
+        'Código': 'Código',
+        'Ano-Período': 'Ano-Período',
+        'Docente': 'Docente',
+        'Qtde Vagas Ofertadas': 'Qtde Vagas Ofertadas',
+        'Qtde Vagas Ocupadas': 'Qtde Vagas Ocupadas',
+        'Local': 'Local'
+      },
+      csvSeparator: ','
+    });
+    file_saver.writeFile("tabelas_csv/" + nome_unidade.replace(/[$&+,:;=?@#|'<>.^*()%!-]/i, ""), data, err => {if (err) {console.log(err)}})
 }
 
 async function run() {
@@ -33,9 +53,9 @@ await page.evaluate((selector) => {
 }, SELECTOR_GRADUACAO)
 
 await page.waitForSelector(SELECTOR_UNIDADE)
-await page.evaluate((selector) => {
+qtd_unidades = await page.evaluate((selector) => {
     let botao_unidade = document.querySelector(selector)
-    lista_unidades = botao_unidade.options
+    return botao_unidade.options.length
 }, SELECTOR_UNIDADE)
 
 await page.waitForSelector(SELECTOR_PERIODO)
@@ -43,7 +63,7 @@ await page.evaluate((selector) => {
     document.querySelector(selector).selectedIndex = 3; //ID semestre de verão
 }, SELECTOR_PERIODO)
 
-for (i = 1; i <= 1; i++ ){
+for (i = 1; i < qtd_unidades; i++ ){
     await scrape_disciplinas(i, page)
 }
 }
